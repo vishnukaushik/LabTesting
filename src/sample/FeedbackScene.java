69985a6887c2e4ce38486c1ddad86a16bcfe72c5
Vishnu Kaushik <vishnukoushik23@gmail.com>
2021-11-07 17:37:47 +0530

    Feedback events added

--- src/sample/FeedbackScene.java
+++ src/sample/FeedbackScene.java
@@ -2,11 +2,16 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
 public class FeedbackScene {
     @FXML
     private javafx.scene.control.Button btn_cancel;
+    @FXML
+    private TextArea textArea;
+
 
     public void cancel(ActionEvent actionEvent){
         Stage stage = (Stage) btn_cancel.getScene().getWindow();
@@ -15,6 +20,10 @@ public class FeedbackScene {
 
     public void submit(ActionEvent actionEvent)
     {
+        String feedback = textArea.getText();
+        System.out.println(feedback);
 //        TODO: write a query to database
+
+        cancel(actionEvent);
     }
 }
