472b7ddde7c32e46faa001467827d81cd9cade10
Vishnu Kaushik <vishnukoushik23@gmail.com>
2021-11-07 16:51:43 +0530

    Timer frontend added

---
+++ src/sample/TempMain.java
@@ -0,0 +1,26 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class TempMain extends Application {
+    public static void StartApplication(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(TimerScene.class.getResource("timerScene.fxml"));
+        primaryStage.setTitle("Timer Page");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        StartApplication(primaryStage);
+    }
+    public static void main(String[] args) {
+        Application.launch();
+    }
+}
---
+++ src/sample/TimerScene.java
@@ -0,0 +1,18 @@
+package sample;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+
+
+public class TimerScene{
+    @FXML private javafx.scene.control.Button btn_cancel;
+
+    public void cancel(ActionEvent actionEvent){
+        Stage stage = (Stage) btn_cancel.getScene().getWindow();
+        stage.close();
+    }
+    public void extendSession(ActionEvent actionEvent){
+//        TODO(manage the time)
+        System.out.println("session extended");
+    }
+}
